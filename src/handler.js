3228afff72499a7b9c1e2976e0e84862c512a33f
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-09-18 22:40:03 +0700

    add editBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -76,7 +76,7 @@ const addBookHandler = (request, h) => {
     status: "error",
     message: "Buku gagal ditambahkan",
   });
-  response.code(500);
+  response.code(404);
   return response;
 };
 
@@ -111,4 +111,80 @@ const getAllBookByIdHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addBookHandler, getAllBookHandler, getAllBookByIdHandler };
+// UpdateBookByIdHandler
+EditBooksByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const updatedAt = new Date().toISOString();
+
+  if (!name) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal memperbaiki buku. Mohon isi nama buku",
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message:
+        "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+    response.code(400);
+    return response;
+  }
+
+  const index = books.findIndex((book) => book.id === id);
+
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: "success",
+      message: "Buku berhasil diperbarui",
+    });
+
+    response.code(200);
+    return response;
+  } else {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal memperbarui buku. Id tidak ditemukan",
+    });
+
+    response.code(404);
+    return response;
+  }
+};
+
+module.exports = {
+  addBookHandler,
+  getAllBookHandler,
+  getAllBookByIdHandler,
+  EditBooksByIdHandler,
+};
